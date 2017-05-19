23788cdf1e6c54ab000a51b981321b1fdb81f664
John Coates <acapulco1988@gmail.com>
2017-05-19 08:06:21 -0700

    don't print coverage

--- Scripts/testWithUICoverageFix.sh
+++ Scripts/testWithUICoverageFix.sh
@@ -17,4 +17,5 @@ echo "Running xcodebuild with lldb to inject fix library"
 xcrun lldb -f /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -s "${SCRIPTS_DIR}/coverageFix.lldb"
 
 # test coverage with:
-xcrun llvm-cov report -instr-profile=./DerivedData/Slate/Build/Intermediates/CodeCoverage/Coverage.profdata ./DerivedData/Slate/Build/Intermediates/CodeCoverage/Products/Debug-iphonesimulator/Slate.app/Slate
+# xcrun llvm-cov report -instr-profile=./DerivedData/Slate/Build/Intermediates/CodeCoverage/Coverage.profdata ./DerivedData/Slate/Build/Intermediates/CodeCoverage/Products/Debug-iphonesimulator/Slate.app/Slate
+exit 0
